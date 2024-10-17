41221d58621c59b705dec2bd647fba0d999887b7
RioBagusRinanto <riobagusrinanto@gmail.com>
2024-10-18 00:00:02 +0700

    add sample controller

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,10 +1,13 @@
 // app/api/[[...slugs]]/route.ts
 import { Elysia, t } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
+import { getData } from '@/app/controller/getDetail'
 
 const app = new Elysia({ prefix: '/api' })
     .use(swagger())
-    .get('/', () => 'hello Next')
+    .get('/', () => {
+        return getData()
+    })
     .post('/', ({ body }) => body, {
         body: t.Object({
             name: t.String()
---
+++ app/controller/getDetail.ts
@@ -0,0 +1,12 @@
+export const getData = () => {
+    const returnData = {
+        message : "Hello World",
+        data : {
+            name : "John",
+            age : 30,
+            occupation : "Software Engineer"
+        }
+    }
+    
+    return returnData;
+}
